51602470f3a7bd7bcf54378cd64d9e90d9cc9bee
bakeza <dbakeza2002@gmail.com>
2024-08-30 15:31:33 +0300

    fix bugs

--- src/client/index.js
+++ src/client/index.js
@@ -3,10 +3,29 @@ import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
 
-import { handleSubmit } from "./js/formHandler";
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("urlForm");
-  if (form) {
-    form.addEventListener("submit", handleSubmit);
-  }
+const CACHE_NAME = "cache-v1";
+const urlsToCache = [
+  "/",
+  "/views/index.html",
+  "/js/formHandler.js",
+  "./styles/base.scss",
+  "./styles/footer.scss",
+  "./styles/form.scss",
+  "./styles/header.scss",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
